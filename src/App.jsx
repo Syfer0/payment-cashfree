c159eb8d1604664144bd5248304e312a55f31f9e
Arman <125900320+Syfer0@users.noreply.github.com>
2024-01-16 20:39:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+function Navbar() {
+  <UsserCard />;
+}
+function UsserCard() {
+  return (
+    <>
+      <div className="logo">
+        <img src="/public/img/logo.png" alt="logo" />
+      </div>
+      <div></div>
+    </>
+  );
+}
+export default App;
